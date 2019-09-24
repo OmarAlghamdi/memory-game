e7d1005135d2c30eff025ce2e981aefe12ca117e
Omar Alghamdi <mr.alghamdi.omar@gmail.com>
2019-09-24 04:22:45 +0300

    no change. jsut commenting and reorder code blocks

--- js/app.js
+++ js/app.js
@@ -1,11 +1,9 @@
-/*
- * Create a list that holds all of your cards
- */
+
+// Card object used to represnt a card and its opetations 
 
 class card {
 
-    constructor(no, isFlipped, isMatched, DOM){
-        this.id = no;
+    constructor(isFlipped, isMatched, DOM){
         this.flipped = isFlipped;
         this.matched = isMatched;
         this.dom = DOM;
@@ -53,7 +51,7 @@ class card {
     
 }
 
-
+// global variables
 let deck = [];
 let deckDOM = null;
 let openedCard = null;
@@ -63,90 +61,28 @@ let timer;
 let winMsg = document.getElementsByClassName('win')[0];
 
 
-function win() {
-    winMsg.style.display = "block";
-
-    clearInterval(timer);
-    document.getElementsByClassName('r-time')[0].textContent = formatTime(time);
-    document.getElementsByClassName('r-moves')[0].textContent = moves;
-    let span = document.getElementsByClassName('star-holder')[0];
-    span.innerHTML = "";
-    for (let i = 0; i < starCount; i++) {
-        const newStar = document.createElement('I');
-        newStar.className = "fa fa-star";
-        span.appendChild(newStar);
-    }
-    
+// setting listeners to handle clicking on new game
+const resetBtns = document.getElementsByClassName('restart');
+for (let i = 0; i < resetBtns.length; i++) {
+    resetBtns[i].addEventListener('click', newGame);    
 }
 
+// start tha game one page loaded
+createDeck();
+newGame();
 
-
-function tick(){
-    time++;
-    
-    document.getElementsByClassName('time')[0].textContent = formatTime(time);
-}
-
-function formatTime(seconds){
-    let timeString = "", mm, ss;
-    mm = (seconds - seconds%60) / 60 ;
-    ss = seconds%60;
-    if(mm < 10)
-        timeString += "0";
-    timeString += mm;
-    timeString += ":";
-    if(ss <10)
-        timeString += "0";
-    timeString += ss;
-    return timeString;
-}
-
+// creating a deck and card objects out of html
 function createDeck(){
     getDOM();
     for (let i = 0; i < 16; i++) {
-        const c = new card(i, false, false, deckDOM[i]);
+        const c = new card(false, false, deckDOM[i]);
         deckDOM[i].addEventListener('click', listener);
         deck.push(c);
     }
 
 }
 
-function listener(e){
-    id = e.target.id;
-    try{
-        deck[id].flip();
-    } catch (e){
-
-    }
-    
-}
-
-
-function incrementMoves(){
-    document.getElementsByClassName('moves')[0].textContent = ++moves;
-    if(moves > 20 && starCount > 1)
-        if(moves % 5 == 0){
-            const stars = document.getElementsByClassName('stars')[0];
-            const star = document.getElementsByTagName('li')[0];
-            stars.removeChild(star);
-            starCount--;
-        }
-}
-function resetMoves(){
-    document.getElementsByClassName('moves')[0].textContent = moves = 0;
-    const stars = document.getElementsByClassName('stars')[0];
-    const star = document.getElementsByTagName('li')[0];
-    
-    for (let i = starCount; i < 5; i++) {
-        const newStar = document.createElement('LI');
-        newStar.innerHTML = star.innerHTML;
-        stars.appendChild(newStar);
-        starCount++;
-    }
-}
-
-
-
+// handle creating new game instance
 function newGame() {
     
     winMsg.style.display = "none";
@@ -170,6 +106,63 @@ function newGame() {
     
 }
 
+// win action method called win the player win to display the winning message
+function win() {
+    winMsg.style.display = "block";
+
+    clearInterval(timer);
+    document.getElementsByClassName('r-time')[0].textContent = formatTime(time);
+    document.getElementsByClassName('r-moves')[0].textContent = moves;
+    let span = document.getElementsByClassName('star-holder')[0];
+    span.innerHTML = "";
+    for (let i = 0; i < starCount; i++) {
+        const newStar = document.createElement('I');
+        newStar.className = "fa fa-star";
+        span.appendChild(newStar);
+    }
+    
+}
+
+
+// card click listenter
+function listener(e){
+    id = e.target.id;
+    try{
+        deck[id].flip();
+    } catch (e){
+
+    }
+    
+}
+
+// handles moves incremtation and star rating
+// star rating is based on number of moves
+function incrementMoves(){
+    document.getElementsByClassName('moves')[0].textContent = ++moves;
+    if(moves > 20 && starCount > 1)
+        if(moves % 5 == 0){
+            const stars = document.getElementsByClassName('stars')[0];
+            const star = document.getElementsByTagName('li')[0];
+            stars.removeChild(star);
+            starCount--;
+        }
+}
+
+// reset both moves to zero and stars to 5 stars when a new game start
+function resetMoves(){
+    document.getElementsByClassName('moves')[0].textContent = moves = 0;
+    const stars = document.getElementsByClassName('stars')[0];
+    const star = document.getElementsByTagName('li')[0];
+    
+    for (let i = starCount; i < 5; i++) {
+        const newStar = document.createElement('LI');
+        newStar.innerHTML = star.innerHTML;
+        stars.appendChild(newStar);
+        starCount++;
+    }
+}
+
+// getting the html represntation of each card 
 function getDOM(){
     const cards = document.getElementsByClassName('card');
     const dom = [];
@@ -179,18 +172,29 @@ function getDOM(){
     deckDOM = dom;
 }
 
+// time handler
+function tick(){
+    time++;
+    
+    document.getElementsByClassName('time')[0].textContent = formatTime(time);
+}
 
+// utility function to format the time as string
+function formatTime(seconds){
+    let timeString = "", mm, ss;
+    mm = (seconds - seconds%60) / 60 ;
+    ss = seconds%60;
+    if(mm < 10)
+        timeString += "0";
+    timeString += mm;
+    timeString += ":";
+    if(ss <10)
+        timeString += "0";
+    timeString += ss;
+    return timeString;
+}
 
-
-
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-
-// Shuffle function from http://stackoverflow.com/a/2450976
+// shuffle the deck of cards
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -204,25 +208,3 @@ function shuffle(array) {
 
     return array;
 }
-
-const resetBtns = document.getElementsByClassName('restart');
-for (let i = 0; i < resetBtns.length; i++) {
-    resetBtns[i].addEventListener('click', newGame);    
-}
-
-
-createDeck();
-newGame();
-
-
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
